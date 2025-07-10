7c261cef7ba2a47fc6744c7c07031624e160c09d
Andresito126 <creed126ax@gmail.com>
2025-07-10 12:26:32 -0600

    Add: merging with feature/login to develop

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,12 @@
 import './index.css'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Login from './features/users/Presentation/Pages/Login/Login'
-import CreateUser from './features/users/Presentation/Pages/Createuser/Createuser'
-
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Login />,
-  },
-  {
-    path: '/createuser',
-    element: <CreateUser></CreateUser>,
-  }
-])
-
+import {RouterProvider } from 'react-router-dom'
+import { navigationWrapper } from './core/navigation/navigationWrapper'
 
 createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
+ <React.StrictMode>
+    <RouterProvider router={navigationWrapper} />
   </React.StrictMode>
 )
+
