2ac7b033180defde9459ee14f642b504807d3cb0
Andresito126 <creed126ax@gmail.com>
2025-07-10 14:11:47 -0600

    Add:giving structure to aside

--- src/features/shared/layouts/asideContainer-layout.tsx
+++ src/features/shared/layouts/asideContainer-layout.tsx
@@ -3,19 +3,26 @@ import { useLocation } from "react-router-dom";
 export const AsideContainerLayout = () => {
   const { pathname } = useLocation();
 
-//to sjow the aside in certain routes (it's a state)
+  //to sjow the aside in certain routes (it's a state)
   const showAsideInRoutes = ["/dashboard"];
 
   if (!showAsideInRoutes.includes(pathname)) return null;
 
   return (
-    <aside className="w-[300px] bg-[#01182B] shadow-lg p-4 hidden lg:block mr-[20px] ">
-      <h2 className="text-lg font-semibold mb-4">Resumen</h2>
-    
-        <p className="text-sm text-gray-700">Device</p>
-        <p className="text-sm text-gray-700">Mascota</p>
-        <p className="text-sm text-gray-700"> Clima</p>
-     
+    <aside className=" flex-col w-[300px] bg-[#01182B] shadow-lg mt-[10px] hidden lg:block mr-[20px] ">
+      <div className="flex flex-col gap-[20px]  h-[10.625rem]">
+        <div className="shadow-lg p-4 h-full bg-[#011521] flex justify-between items-center px-[20px] py-[20px] text-white rounded-[20px]">
+          <h1>Usuario</h1>
+        </div>
+        <div className="shadow-lg p-4 h-full bg-[#011521] flex justify-between items-center px-[20px] py-[20px] text-white rounded-[20px]">
+          <h1>Device </h1>
+        </div>
+      </div>
+ 
+
+      <p className="text-sm text-gray-700">Device</p>
+      <p className="text-sm text-gray-700">Mascota</p>
+      <p className="text-sm text-gray-700"> Clima</p>
     </aside>
   );
 };
