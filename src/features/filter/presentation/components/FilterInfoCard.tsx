dc8842d17c07b7042191c285bdd7900a2b0175cf
Andresito126 <creed126ax@gmail.com>
2025-07-16 17:36:35 -0600

    Fix:icons and views fixed

--- src/features/filter/presentation/components/FilterInfoCard.tsx
+++ src/features/filter/presentation/components/FilterInfoCard.tsx
@@ -1,3 +1,5 @@
+import CircularProgressIcon from "../../../shared/components/generic/CircularProgressIcon";
+
 type Props = {
   name: string;
   value: number;
@@ -16,8 +18,15 @@ export const FilterInfoCard = ({ name, value, status, date }: Props) => {
     <div className="shadow-lg  dark:bg-[#011521] border-[#CBD5E1] dark:border-[#105B85] border-[1px]  p-4 rounded-xl  w-full h-[120px] flex flex-col justify-center">
       <h3 className="text-[20px] font-bold">{name}</h3>
       <div className="flex items-center gap-2 text-sm">
-        <span className="text-[18px] font-semibold">🔧 {value}</span>
-        <span className={` text-[16px]  text-semibold px-2 rounded-[20px] ${statusColors[status]}`}>
+        <div className="flex items-center gap-1">
+          <CircularProgressIcon progress={value} size={48} />
+          <span className="text-[16px] font-semibold dark:text-white">
+            {value}%
+          </span>
+        </div>
+        <span
+          className={`text-[16px] text-semibold px-2 rounded-[20px] ${statusColors[status]}`}
+        >
           {status}
         </span>
       </div>
